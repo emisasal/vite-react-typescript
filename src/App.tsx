3ef25261c3739c130de89d4355e6f51126cac251
emisasal <emiliano.sasal@gmail.com>
2024-10-07 15:01:25 -0300

    Icons update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header'
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
+import Header from "./components/Header"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,10 +11,10 @@ function App() {
     <>
       <div>
         <Header />
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
